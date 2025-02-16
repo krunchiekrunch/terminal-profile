947ae4a25446466a95ae10a0502c1161a154b914
krunch <krunch.hackclub.app>
2025-02-16 21:03:22 +0000

    prod

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Vine 
-This website is for a new potential YSWS, called Vine.   
-In essence, you make a piece of music using an open source, code based music development software of your choice. Muse/Sonic Pi recommended 
-Once you've finished, you submit your work, and get a 5' Vinyl with your music on it! (60 seconds to 4 minutes) 
+# Profile Website
 
-A link to the current website can be found here  
-https://vineysws.vercel.app/
-
-Thanks for visiting this repo!
+Originally made by [@N1k0s1](https://github.com/N1k0s1/Vine)
--- src/js/Terminal.jsx
+++ src/js/Terminal.jsx
@@ -75,7 +75,7 @@ function Terminal() {
     while (id--) {
       clearInterval(id);
     }
-    setText1("ssh guest@vine.me");
+    setText1("ssh guest@krunch.local");
     setText3("Access Granted!");
   }
 
@@ -92,34 +92,30 @@ function Terminal() {
           while (id--) {
             clearInterval(id);
           }
-          setText1("ssh guest@vine.me");
-          setText2("guest@vine.me's password:");
+          setText1("ssh guest@krunch.local");
+          setText2("guest@krunch.local's password:");
           setText3("Access Granted!");
         }
         const CommandArea = document.getElementById("command");
         if (CommandArea) {
           previousCommand = CommandArea.value;
-          setprevusedCommand((prevArray) => [...prevArray, "guest@vine.me:~$ " + previousCommand]);
+          setprevusedCommand((prevArray) => [...prevArray, "guest@krunch.local:~$ " + previousCommand]);
           if (CommandArea.value === "github") {
-            window.open("https://github.com/N1k0s1/vine-ysws-website", '_blank');
+            window.open("https://github.com/RadioactivePotato", '_blank');
           } else if (CommandArea.value === "source") {
-            window.open("https://github.com/N1k0s1/vine-ysws-website", '_blank');
-          }
-          else if (CommandArea.value === "guide") {
-            window.open("https://sonic-pi.mehackit.org/exercises/en/01-introduction/01-introduction.html", '_blank');
-          }
-          else if (CommandArea.value === "submit") {
-            window.open("https://forms.fillout.com/t/tZ1CDPD5Brus", '_blank');
+            window.open("https://github.com/RadioactivePotato/krunch-site", '_blank');
+          } else if (CommandArea.value === "discord") {
+            window.open("https://discordapp.com/users/1166013268008120340", '_blank');
           }
           CommandArea.value = "";
         }
       }
     });
 
-    Typewriter("ssh guest@vine.me", 100, setText1);
+    Typewriter("ssh guest@krunch.local", 100, setText1);
 
     setTimeout(() => {
-      setText2("guest@vine.me's password:▮");
+      setText2("guest@krunch.local's password:▮");
     }, 3000);
 
     setTimeout(() => {
@@ -127,11 +123,11 @@ function Terminal() {
     }, 4300);
 
     setTimeout(() => {
-      setText3("Connecting to guest@vine.me...");
+      setText3("Connecting to guest@krunch.local...");
     }, 4300);
 
     setTimeout(() => {
-      setText2("guest@vine.me's password:");
+      setText2("guest@krunch.local's password:");
       setText3("> Access granted.");
     }, 7300);
   }, []);
@@ -139,7 +135,7 @@ function Terminal() {
   return (
     <div className="terminal">
       <div className='console'>
-        <span className='userPrefix'>user@vine:~$
+        <span className='userPrefix'>user@raspberrypi:~$
           <span style={{ color: "white", marginLeft: "8px" }}>{Text1}{Text1.length === 20 ? "" : cursor}</span>
         </span>
 
@@ -150,12 +146,13 @@ function Terminal() {
         {Text3.includes("Access") ? (
 <pre>
 {`
-____   ____.__               
-\\   \\ /   /|__| ____   ____  
- \\   Y   / |  |/    \\_/ __ \\ 
-  \\     /  |  |   |  \\  ___/ 
-   \\___/   |__|___|  /\\___  >
-                   \\/     \\/ 
+
+██╗░░██╗██████╗░██╗░░░██╗███╗░░██╗░█████╗░██╗░░██╗
+██║░██╔╝██╔══██╗██║░░░██║████╗░██║██╔══██╗██║░░██║
+█████═╝░██████╔╝██║░░░██║██╔██╗██║██║░░╚═╝███████║
+██╔═██╗░██╔══██╗██║░░░██║██║╚████║██║░░██╗██╔══██║
+██║░╚██╗██║░░██║╚██████╔╝██║░╚███║╚█████╔╝██║░░██║
+╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝
 `}
 </pre>
 
@@ -164,8 +161,8 @@ ____   ____.__
         {Text3.includes("Access") ? <span>Welcome!</span> : ""}
         {Text3.includes("Access") ? <span></span> : ""}<br />
         {Text3.includes("Access") ? <span><span style={{ color: "skyblue" }}>Available Commands:</span></span> : ""}
-        {Text3.includes("Access") ? <span><span style={{ color: "#c9c9c9" }}>General: </span> about, hacksim, neofetch, clear, instructions</span> : ""}
-        {Text3.includes("Access") ? <span><span style={{ color: "#c9c9c9" }}>Links:</span> github, submit, guide</span> : ""}
+        {Text3.includes("Access") ? <span><span style={{ color: "#c9c9c9" }}>General: </span> about, hacksim, neofetch, clear</span> : ""}
+        {Text3.includes("Access") ? <span><span style={{ color: "#c9c9c9" }}>Links:</span> github, source, discord</span> : ""}
 
         <br></br>
         {Text3.includes("Access") ? <span>Thank you for visiting!◝(ᵔᵕᵔ)◜</span> : ""}
@@ -173,16 +170,13 @@ ____   ____.__
         <ul className='previousCommands' id='console23'>
           {prevusedCommand.map((item, index) => {
             if (item.match(new RegExp(`\\b${"github"}\\b`, 'g'))) {
-              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened GitHub https://github.com/N1k0s1/vine-ysws-website</span><br></br><br></br></li>;
+              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the URL to my GitHub in a new tab: https://github.com/RadioactivePotato</span><br></br><br></br></li>;
             }
             else if (item.match(new RegExp(`\\b${"source"}\\b`, 'g'))) {
-              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the source code of this site in a new tab: https://github.com/N1k0s1/vine-ysws-website</span><br></br><br></br></li>;
+              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the source code of this site in a new tab: https://github.com/RadioactivePotato/krunch-site</span><br></br><br></br></li>;
             }
-            else if (item.match(new RegExp(`\\b${"guide"}\\b`, 'g'))) {
-              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the guide for Sonic Pi in a new tab: https://sonic-pi.mehackit.org/exercises/en/01-introduction/01-introduction.html</span><br></br><br></br></li>;
-            }
-            else if (item.match(new RegExp(`\\b${"submit"}\\b`, 'g'))) {
-              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the submission form in a new tab: https://forms.fillout.com/t/tZ1CDPD5Brus</span><br></br><br></br></li>;
+            else if (item.match(new RegExp(`\\b${"discord"}\\b`, 'g'))) {
+              return <li key={index}>{item}<br></br><br></br><span style={{ color: "#c9c9c9" }}>Opened the URL to my Discord in a new tab: https://discordapp.com/users/1166013268008120340</span><br></br><br></br></li>;
             }
 
             else if (item.match(new RegExp(`\\b${"hacksim"}\\b`, 'g'))) {
@@ -192,7 +186,7 @@ ____   ____.__
             }
             else if (item.match(new RegExp(`\\b${"aborthack"}\\b`, 'g'))) {
               return <div key={index}><li>{item}</li>
-                bash: {item.replace("guest@renisal.me:~$", '')}: ERROR - Script terminated by the user</div>;
+                bash: {item.replace("guest@hacker.me:~$", '')}: ERROR - Script terminated by the user</div>;
             }
             else if (item.match(new RegExp(`\\b${"clear"}\\b`, 'g'))) {
               return setprevusedCommand([]);
@@ -200,13 +194,7 @@ ____   ____.__
             else if (item.match(new RegExp(`\\b${"about"}\\b`, 'g'))) {
               return <div><li key={index}>{item}</li>
                 <div className='about'><br></br>
-                to take part in this YSWS, you will code a piece of music that's at least 60 seconds long (max 4 minutes) using any open source code based music development software. (link to it on your github!) once you have completed your piece, you can submit it to the form provided. all submissions will recieve a vinyl with their piece of music on it. (this depends if there are at least 50 submissions.) the best pieces will be featured on the website. Good Luck! <br></br><br></br>
-                </div></div>
-            }
-            else if (item.match(new RegExp(`\\b${"instructions"}\\b`, 'g'))) {
-              return <div><li key={index}>{item}</li>
-                <div className='instructions'><br></br>
-                once you've made your 60 sec - 4 min song, make a GitHub repo, and a short readme about the genre/what the song's about. then submit your project using the "submit" command!<br></br><br></br>
+                Welcome to my website!<br></br><br></br>
                 </div></div>
             }
              else if (item.match(new RegExp(`\\b${"neofetch"}\\b`, 'g'))) {
@@ -214,52 +202,54 @@ ____   ____.__
                 <div className='neofetch'><br></br>
                 <div style={{ display: 'flex' }}>
                 {/* ASCII Art on the left */}
-                <div style={artStyle}>
-                  ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣾⣿⣿⣿⣿⣷⣶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀<br />
-                  ⠀⠀⠀⠀⠀⣠⢔⣫⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀<br />
-                  ⠀⠀⣠⢊⡴⡫⢚⡽⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀<br />
-                  ⠀⠀⡴⣱⢫⢎⡔⡩⣚⠵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀<br />
-                  ⠀⣼⣽⣳⣣⢯⣞⡜⡱⣫⢷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀<br />
-                  ⢸⣿⣿⣿⣿⣿⣿⣾⡽⣱⣫⠞⠉⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇<br />
-                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠃⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿<br />
-                  ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠘⠃⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿<br />
-                  ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿<br />
-                  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⣀⣀⣠⣴⢟⡵⣳⢯⢿⣿⡟⣿⣿⣿⣿⡇<br />
-                  ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣞⡵⣫⢏⢞⡽⡽⣻⢯⡟⠀<br />
-                  ⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣚⢕⡡⢊⠜⡵⣣⠟⠀⠀<br />
-                  ⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢷⣫⢖⡥⢊⡴⠋⠀⠀⠀⠀⠀<br />
-                  ⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣞⣭⠞⠋⠀⠀⠀⠀⠀⠀<br />
-                  ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀
-                </div>
+                <div style={artStyle}>{`
+        _,met$$$$$gg.     
+      ,g$$$$$$$$$$$$$$$P.     
+    ,g$$P""       """Y$$.".     
+  ,$$P'              \`$$$.       
+',$$P       ,ggs.     \`$$b:     
+\`d$$'     ,$P"'   .    $$$     
+  $$P      d$'     ,    $$P
+  $$:      $$.   -    ,d$$'
+  $$;      Y$b._   _,d$P'
+  Y$$.    \`.\`"Y$$$$P"' 
+  \`$$b      "-.__
+  \`Y$$b
+    \`Y$$.
+      \`$$b.
+        \`Y$$b.
+          \`"Y$b._
+              \`""""
+             `}</div>
 
                 {/* Terminal text on the right */}
                 <div style={terminalStyle}>
-                  <span style={{ color: "#33FF57" }}>guest@vine.me</span><br />
+                  <span style={{ color: "#33FF57" }}>guest@krunch.local</span><br />
                   -------------------------<br />
-                  <span style={{ color: "#33FF57" }}>OS:</span> Sonic Pi π<br />
-                  <span style={{ color: "#33FF57" }}>Host:</span> Vine v1.0<br />
-                  <span style={{ color: "#33FF57" }}>Kernel:</span> 6.1.0-21-amd64<br />
-                  <span style={{ color: "#33FF57" }}>Uptime:</span> 21,373,712  mins<br />
+                  <span style={{ color: "#33FF57" }}>OS:</span>  Debian GNU/Linux 12 (bookworm) aarch64<br />
+                  <span style={{ color: "#33FF57" }}>Host:</span> Raspberry Pi 3 Model B Rev 1.2<br />
+                  <span style={{ color: "#33FF57" }}>Kernel:</span> Linux 6.6.74+rpt-rpi-v8<br />
+                  <span style={{ color: "#33FF57" }}>Uptime:</span> 1 day, 18 hours, 24 mins<br />
                   <span style={{ color: "#33FF57" }}>Resolution:</span> 1920x1080<br />
                   <span style={{ color: "#33FF57" }}>DE:</span> GNOME 43.9 (wayland)<br />
-                  <span style={{ color: "#33FF57" }}>WM:</span> Mutter<br />
-                  <span style={{ color: "#33FF57" }}>Theme:</span> Adwaita [GTK2/3]<br />
-                  <span style={{ color: "#33FF57" }}>Terminal:</span> gnome-terminal<br />
-                  <span style={{ color: "#33FF57" }}>CPU:</span> Intel 80286 (1) @ 12.5MHz<br /><br />
+                  <span style={{ color: "#33FF57" }}>WM:</span> Openbox (X11)<br />
+                  <span style={{ color: "#33FF57" }}>Theme:</span> Clearlooks<br />
+                  <span style={{ color: "#33FF57" }}>Terminal:</span> /dev/pts/0<br />
+                  <span style={{ color: "#33FF57" }}>CPU:</span> BCM2837 (4) @ 1.20 GHz<br />
+                  <span style={{ color: "#33FF57" }}>GPU:</span> Broadcom bcm2835-vc4 [Integrated]<br />
                   
-                  <span style={{color: "#FFFF00"}}>Fun fact!</span> Most climbing mishaps happen from exhaustion.<br />
-                  Remember to take regular breaks!<br /><br />
+                  <span style={{color: "#FFFF00"}}>Message of the Day:</span> Hello World!<br /><br />
                 </div>
                 </div>
                 </div>
                 </div>
             } else {
               return <div><li key={index}>{item}</li>
-                bash: {item.replace("guest@renisal.me:~$", '')}: command not found</div>;
+                bash: {item.replace("guest@hacker.me:~$", '')}: command not found</div>;
             }
           })}
         </ul>
-        {Text3.includes("Access") ? <span className='commands'><span className='userPrefix'>guest@vine.me:~$</span> <input type="text" id="command" name="command" autoFocus></input></span> : ""}
+        {Text3.includes("Access") ? <span className='commands'><span className='userPrefix'>guest@krunch.local:~$</span> <input type="text" id="command" name="command" autoFocus></input></span> : ""}
       </div>
     </div>
   );
